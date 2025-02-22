570f6a25afd2f1a5612b7651bf01ddee95e4e8c7
Omkar399 <omkarpodey@gmail.com>
2025-02-22 12:30:31 -0800

    gitignore and remove secret

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-123'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///ai_hub.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GITHUB_API_TOKEN = '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'
+    GITHUB_API_TOKEN = ''
