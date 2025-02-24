1df9d48b33d16c3e48205af06d47cb14a57950ee
Omkar399 <omkarpodey@gmail.com>
2025-02-23 22:57:39 -0800

    rev secret

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-123'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///ai_hub.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GITHUB_API_TOKEN ='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'
+    GITHUB_API_TOKEN =''
